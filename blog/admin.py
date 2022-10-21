6c53c08e831d7ab3f4c9b4cb78e0bf6b615065c0
DanTaylor28 <dan.taylor1493@gmail.com>
2022-10-21 19:01:10 +0000

    Add post model and customise the admin panel on admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_on', 'updated_on',)
+    list_filter = ('published_on',)
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'author',)
