bbaada37c55d79993be45e95fe97c5c6dc8b0daf
DanTaylor28 <dan.taylor1493@gmail.com>
2022-10-21 14:07:00 +0000

    Create the Post model with all required fields

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=300, unique=True)
+    tag = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog')
+    published_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)
+    body = models.TextField()
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    primary_image = CloudinaryField('image', default='placeholder')
+
+    class Meta:
+        ordering = ['-published_on']
+
+    def __str__(self):
+        return f"{self.title} | {self.author}"
+
+    def num_of_likes(self):
+        return self.likes.count()
