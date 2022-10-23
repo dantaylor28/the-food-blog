42a0b60e5e268e0730c80e3aa6bca1fc8993ab6c
DanTaylor28 <dan.taylor1493@gmail.com>
2022-10-23 17:21:58 +0000

    Create url path to create_post

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('<slug:slug>/', views.PostView.as_view(), name='post_view'),
+    path('create_post/', views.CreateView.as_view(), name='create_post'),
 ]
