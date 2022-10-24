8de69d220e84b1dfa350f2921d15b85f5e324e8e
DanTaylor28 <dan.taylor1493@gmail.com>
2022-10-24 14:32:20 +0000

    Add url for delete_post.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, PostView, AddView, UpdatePostView
+from .views import HomeView, PostView, AddView, UpdatePostView, DeleteView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('<slug:slug>/', PostView.as_view(), name='post_view'),
     path('create/post/', AddView.as_view(), name='create_post'),
     path('edit/<slug:slug>/', UpdatePostView.as_view(), name='edit_post'),
+    path('delete/<slug:slug>/', DeleteView.as_view(), name='delete_post')
 ]
