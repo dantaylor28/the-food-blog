7a47cec4084263a31aff68c1924f1b7bf3a56491
DanTaylor28 <dan.taylor1493@gmail.com>
2022-10-24 13:12:10 +0000

    Add url for the UpdatePostView

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, PostView, AddView
+from .views import HomeView, PostView, AddView, UpdatePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('<slug:slug>/', PostView.as_view(), name='post_view'),
     path('create/post/', AddView.as_view(), name='create_post'),
+    path('<slug:slug>/edit/', UpdatePostView.as_view(), name='edit_post'),
 ]
