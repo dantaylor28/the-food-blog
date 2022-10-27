a78f063f121f6c712eb68fa021b697ee2975bbc1
DanTaylor28 <dan.taylor1493@gmail.com>
2022-10-27 16:50:12 +0000

    Add some formatting to the comments section

--- blog/models.py
+++ blog/models.py
@@ -39,3 +39,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.post.title} | {self.name}"
+
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post, Comment
 from .forms import PostForm, EditForm, CommentForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 
 class HomeView(generic.ListView):
