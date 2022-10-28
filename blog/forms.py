cdd5c0dd2c630e29ac41b6cd1d300d857db37ee6
DanTaylor28 <dan.taylor1493@gmail.com>
2022-10-28 07:50:59 +0000

    Add js to create_post.html to auto assign the author value to the form so users cannot post from anyone other than themselves

--- blog/forms.py
+++ blog/forms.py
@@ -12,8 +12,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Blog Title:'}),
             'tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'slug': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control',}),
+            'author': forms.TextInput(attrs={'id': 'author', 'value': '', 'type': 'hidden'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Share Your Content Here:'}),
         }
 
