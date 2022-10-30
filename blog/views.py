4dbeca90b26285d69de741e054033e5b7e6bf15e
DanTaylor28 <dan.taylor1493@gmail.com>
2022-10-30 13:00:08 +0000

    Add __iexact to my CategoryView so the url recognises upper and lower case category names

--- blog/views.py
+++ blog/views.py
@@ -78,5 +78,5 @@ class LikeView(View):
 # 'category_page.html'
 
 def CategoryView(request, type):
-    posts = Post.objects.filter(category=type)
+    posts = Post.objects.filter(category__iexact=type)
     return render(request, 'category_page.html', {'type': type, 'posts': posts})
